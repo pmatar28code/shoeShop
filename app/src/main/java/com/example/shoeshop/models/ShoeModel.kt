fab16af5a4e8ff0621e13ea626426ace25a0155d
pmatar28code <78227811+pmatar28code@users.noreply.github.com>
2021-02-09 11:49:15 -0600

    layout, created model package, repository package, model data class, repository class
    layout, created model package, repository package, model data class, repository class

---
+++ app/src/main/java/com/example/shoeshop/models/ShoeModel.kt
@@ -0,0 +1,9 @@
+package com.example.shoeshop.models
+
+data class ShoeModel(
+        val brand:String,
+        val model:String,
+        val price:String,
+        val image:Int
+
+)
---
+++ app/src/main/java/com/example/shoeshop/repository/ShoeShopRepository.kt
@@ -0,0 +1,4 @@
+package com.example.shoeshop.repository
+
+class ShoeShopRepository {
+}
